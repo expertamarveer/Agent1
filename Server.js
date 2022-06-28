0ddc72cd2c865ae510c554a2ed7bf099ce420f6e
Amar Veer <amar.veer@expleogroup.com>
2022-06-28 12:57:40 +0530

    all commit first

---
+++ Server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require("appdynamics").profile({
+
+  controllerHostName: 'siemensil.saas.appdynamics.com',
+  controllerPort: 443,
+  controllerSslEnabled: true,  // Set to true if controllerPort is SSL
+  accountName: 'siemensil',
+  accountAccessKey: '2ili1qt6b0eu', //required
+
+  applicationName: 'agent',
+
+  tierName: 'yairagent',
+
+  nodeName: 'yairagent'
+
+ });
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+//const HOST = '0.0.0.0';
+const HOST = '192.168.0.177';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
